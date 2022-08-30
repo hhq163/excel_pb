e8db489abb86a3a7e0979865f738b885f19712e4
hhq163 <hhq163@gmail.com>
2022-08-30 12:09:47 +0800

    fix bug on build

--- example/go.mod
+++ example/go.mod
@@ -2,7 +2,7 @@ module test.go
 
 go 1.17
 
-require github.com/hhq163/excel_pb v0.0.0-20220830034008-ac8d5b2fca8b
+require github.com/hhq163/excel_pb v0.0.0-20220830035402-e5970adc265d
 
 require (
 	github.com/golang/protobuf v1.4.3 // indirect
--- example/main.go
+++ example/main.go
@@ -5,18 +5,20 @@ import (
 	"os"
 	"time"
 
+	"github.com/hhq163/excel_pb"
 	"github.com/hhq163/excel_pb/impl"
 )
 
 func main() {
 	loginit()
 
-	impl.ExcelToProto()
-	impl.ProtoToBytes()
+	impl.ExcelToProto(excel_pb.GetExecpath()+"/input", excel_pb.GetExecpath()+"/output", 3)
+	impl.ProtoToBytes(excel_pb.GetExecpath()+"/input", excel_pb.GetExecpath()+"/output")
 	log.Println("create file success !")
 
 }
 
+//日志目录初始化
 func loginit() {
 	file := "./logs/" + "hub" + time.Now().Format("20060102") + ".log"
 	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
